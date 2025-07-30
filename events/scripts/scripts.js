6025b5b4d80895b9ddbab7e3743eb77e2bacdd29
Sharmee Biswas <sharmeeadobe@gmail.com>
2025-07-30 21:39:11 +0530

    revert loading styles.css

--- events/scripts/scripts.js
+++ events/scripts/scripts.js
@@ -86,7 +86,8 @@ export default function decorateArea(area = document) {
 const prodDomains = ['milo.adobe.com', 'business.adobe.com', 'www.adobe.com', 'news.adobe.com', 'helpx.adobe.com'];
 
 // Add project-wide style path here.
-const STYLES = [`${window.location.origin}/events/styles/styles.css`];
+// const STYLES = [`${window.location.origin}/events/styles/styles.css`];
+const STYLES = [];
 
 
 // Add any config options.
