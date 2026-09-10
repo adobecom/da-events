994edf9b230a91245a02ab36dbd9fb39013d5f34
hnvadobe <hnv@adobe.com>
2026-09-10 13:38:39 +0530

    removed the registration code

--- events/scripts/scripts.js
+++ events/scripts/scripts.js
@@ -14,7 +14,6 @@ import {
   LIBS,
   EVENT_LIBS,
 } from './utils.js';
-import { exposeRegistrationStatus, setEventOriginCookie } from './registration-cache.js';
 
 const E_CONFIG = { cmsType: 'DA' };
 const EVENT_BLOCKS_OVERRIDE = [
@@ -27,8 +26,6 @@ const [{
   loadLana,
   getLocale,
   getConfig,
-  loadIms,
-  isSignedOut,
 }, {
   setEventConfig,
   decorateEvent,
@@ -243,16 +240,6 @@ const CONFIG = {
 
 const MILO_CONFIG = setConfig({ ...CONFIG });
 setEventConfig(E_CONFIG, MILO_CONFIG);
-
-// loadIms() memoizes internally, so downstream MEP/GNAV/martech calls reuse this promise.
-loadIms().catch(() => {});
-
-// Fire-and-forget, doesn't block decorateArea/loadPage - see registration-cache.js.
-const eventCode = getMetadata('event-code');
-if (eventCode) {
-  setEventOriginCookie();
-  exposeRegistrationStatus(eventCode, { isSignedOut, getConfig, loadIms });
-}
 
 replaceDotMedia(document);
 
